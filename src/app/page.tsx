7e47ba4b7dc9af50c910a4584bb6d603abfc760e
Noah Carrier <67935880+noahcarrier@users.noreply.github.com>
2024-03-02 16:08:51 -0500

    Replaced squares with cards
    - cats included

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -39,10 +39,39 @@ export default function Home() {
             </div>
           </div>
           <div className="flex justify-between mt-8" id="lesson-prev-boxes">
-            <div className="w-48 h-48 bg-cyan-700 rounded-3xl"></div>
-            <div className="w-48 h-48 bg-cyan-700 rounded-3xl"></div>
-            <div className="w-48 h-48 bg-cyan-700 rounded-3xl"></div>
-            <div className="w-48 h-48 bg-cyan-700 rounded-3xl"></div>
+            
+            <div className="w-48 h-48">
+              <div className="max-w-fit max-h-fit bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                <img className="rounded-t-lg" src="https://www.the-sun.com/wp-content/uploads/sites/6/2023/10/www-instagram-com-monkeycatluna-hl-851711797.jpg" width = "256px" height = "256px" alt="" />
+                <div className="p-3">
+                  <h5 className="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">Memory</h5>
+                </div>
+              </div>
+            </div>
+            <div className="w-48 h-48">
+              <div className="max-w-fit max-h-fit bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                <img className="rounded-t-lg" src="https://imgflip.com/s/meme/Cute-Cat.jpg" width = "256px" height = "256px" alt="" />
+                <div className="p-3">
+                  <h5 className="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">Mastery</h5>
+                </div>
+              </div>
+            </div>
+            <div className="w-48 h-48">
+              <div className="max-w-fit max-h-fit bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                <img className="rounded-t-lg" src="https://i1.sndcdn.com/avatars-000703303372-k246pt-t240x240.jpg" width = "256px" height = "256px" alt="" />
+                <div className="p-3">
+                  <h5 className="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">Strategy</h5>
+                </div>
+              </div>
+            </div>
+            <div className="w-48 h-48">
+              <div className="max-w-fit max-h-fit bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                <img className="rounded-t-lg" src="https://preview.redd.it/r3lcc7gf64791.png?width=540&format=png&auto=webp&s=9d32ec46f884486fd59dfd01453bf5d07bf30d75" width = "256px" height = "256px" alt="" />
+                <div className="p-3">
+                  <h5 className="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">Sprints & Breaks</h5>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
