23d2a73433833bc51dfd8319b10a11a18c643ced
Cristian Suarez <suarezcristian137@gmail.com>
2024-03-01 21:17:59 -0500

    Updated Navbar component to use react.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,14 @@
 import Image from "next/image";
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import NavBar from '../navBar';
+import Navbar from '../components/Navbar';
 
 
 
 export default function Home() {
   return (
     <main className="bg-white">
-      <NavBar />
+      <Navbar />
     </main>
   );
   
