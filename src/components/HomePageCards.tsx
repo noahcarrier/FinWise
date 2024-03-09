1cd409a8c6e4f413ef371de765f3249aecebf66c
Cristian Suarez <suarezcristian137@gmail.com>
2024-03-08 20:56:15 -0500

    Adjusted styling in homepage

---
+++ src/components/HomePageCards.tsx
@@ -0,0 +1,90 @@
+
+export default function HomePageCards() {
+    return (
+    <div className="grid-cols-2 lg:grid xl:grid-cols-4 ">
+        <div className="mx-9 mb-12 flex flex-col rounded-lg bg-yellow-300 text-surface text-black ">
+            <img className="rounded-t-lg object-cover" src="https://www.the-sun.com/wp-content/uploads/sites/6/2023/10/www-instagram-com-monkeycatluna-hl-851711797.jpg" alt="" />
+            <div className="p-6">
+            <h5 className="mb-2 text-3xl font-medium leading-tight">Memory</h5>
+            {/* <p className="mb-4 text-base">
+                This is a longer card with supporting text below as a natural
+                lead-in to additional content. This content is a little bit
+                longer.
+            </p> */}
+            </div>
+        </div>
+
+        <div className="mx-9 mb-12 flex flex-col  rounded-lg bg-yellow-300 text-surface text-black ">
+            <img className="rounded-t-lg object-cover" src="https://imgflip.com/s/meme/Cute-Cat.jpg" alt="" />
+            <div className="p-6">
+            <h5 className="mb-2 text-3xl font-medium leading-tight">Mastery</h5>
+            {/* <p className="mb-4 text-base">
+                This is a longer card with supporting text below as a natural
+                lead-in to additional content. This content is a little bit
+                longer.
+            </p> */}
+            </div>
+        </div>
+
+        <div className="mx-9 mb-12 flex flex-col  rounded-lg bg-yellow-300 text-surface text-black ">
+            <img className="rounded-t-lg object-cover" src="https://i1.sndcdn.com/avatars-000703303372-k246pt-t240x240.jpg" alt="" />
+            <div className="p-6 ">
+            <h5 className="mb-2 text-3xl font-medium leading-tight">Strategy</h5>
+            {/* <p className="mb-4 text-base">
+                This is a longer card with supporting text below as a natural
+                lead-in to additional content.
+            </p> */}
+            </div>
+        </div>
+
+        <div className="mx-9 mb-12 flex flex-col  rounded-lg bg-yellow-300 text-surface text-black ">
+            <img className="rounded-t-lg object-cover" src="https://preview.redd.it/r3lcc7gf64791.png?width=540&format=png&auto=webp&s=9d32ec46f884486fd59dfd01453bf5d07bf30d75" alt="" />
+            <div className="p-6">
+            <h5 className="mb-2 text-3xl font-medium leading-tight">Sprints & Breaks</h5>
+            {/* <p className="mb-4 text-base">
+                This is a longer card with supporting text below as a natural
+                lead-in to additional content. This content is a little bit
+                longer.
+            </p> */}
+            </div>
+        </div>
+
+    </div>
+
+            // <div className="flex justify-between mt-8" id="lesson-prev-boxes">
+            
+        //     <div className="w-48 h-48">
+        //       <div className="max-w-fit max-h-fit bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+        //         <img className="rounded-t-lg" src="https://www.the-sun.com/wp-content/uploads/sites/6/2023/10/www-instagram-com-monkeycatluna-hl-851711797.jpg" width = "256px" height = "256px" alt="" />
+        //         <div className="p-3">
+        //           <h5 className="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">Memory</h5>
+        //         </div>
+        //       </div>
+        //     </div>
+        //     <div className="w-48 h-48">
+        //       <div className="max-w-fit max-h-fit bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+        //         <img className="rounded-t-lg" src="https://imgflip.com/s/meme/Cute-Cat.jpg" width = "256px" height = "256px" alt="" />
+        //         <div className="p-3">
+        //           <h5 className="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">Mastery</h5>
+        //         </div>
+        //       </div>
+        //     </div>
+        //     <div className="w-48 h-48">
+        //       <div className="max-w-fit max-h-fit bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+        //         <img className="rounded-t-lg" src="https://i1.sndcdn.com/avatars-000703303372-k246pt-t240x240.jpg" width = "256px" height = "256px" alt="" />
+        //         <div className="p-3">
+        //           <h5 className="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">Strategy</h5>
+        //         </div>
+        //       </div>
+        //     </div>
+        //     <div className="w-48 h-48">
+        //       <div className="max-w-fit max-h-fit bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+        //         <img className="rounded-t-lg" src="https://preview.redd.it/r3lcc7gf64791.png?width=540&format=png&auto=webp&s=9d32ec46f884486fd59dfd01453bf5d07bf30d75" width = "256px" height = "256px" alt="" />
+        //         <div className="p-3">
+        //           <h5 className="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">Sprints & Breaks</h5>
+        //         </div>
+        //       </div>
+        //     </div>
+        // </div>
+    );
+}
