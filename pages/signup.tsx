b92a2c883ca94683b2551484091f3e3219792071
Kaevhi <ktotran27@gmail.com>
2024-03-14 12:00:52 -0400

    handling form submition

--- pages/signup.tsx
+++ pages/signup.tsx
@@ -1,1 +1,16 @@
 import React, { useState } from 'react';
+
+const Signup = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+
+    const Submition = (e: React.FormEvent) => {
+        e.preventDefault(); // this make sit so that the apge doesn't reload after submitting the form
+        console.log('Form Submitting:', { email, password, confirmPassword });
+        setEmail('');
+        setPassword('');
+        setConfirmPassword('');
+    };
+
+}
