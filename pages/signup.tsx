d355261c9a5c28c0cf78a132556d1de9a31a5197
Kaevhi <ktotran27@gmail.com>
2024-03-14 12:55:40 -0400

    added rough draft of signup screen

--- pages/signup.tsx
+++ pages/signup.tsx
@@ -12,5 +12,46 @@ const Signup = () => {
         setPassword('');
         setConfirmPassword('');
     };
+    
+    return (
+        <div>
+          <h1>Sign Up</h1>
+          <form onSubmit={Submition}>
+            <div>
+              <label htmlFor="email">Email:</label>
+              <input
+                type="email"
+                id="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+              />
+            </div>
+            <div>
+              <label htmlFor="password">Password:</label>
+              <input
+                type="password"
+                id="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+            </div>
+            <div>
+              <label htmlFor="confirmPassword">Confirm Password:</label>
+              <input
+                type="password"
+                id="confirmPassword"
+                value={confirmPassword}
+                onChange={(e) => setConfirmPassword(e.target.value)}
+                required
+              />
+            </div>
+            <button type="submit">Sign Up</button>
+          </form>
+        </div>
+      );
+    };
+    
 
-}
+export default Signup;
