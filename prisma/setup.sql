b175184034a15033d3332167b928c2b5781fc0d6
Noah Carrier <67935880+noahcarrier@users.noreply.github.com>
2024-03-22 20:25:58 -0400

    added lesson & question tables

--- prisma/setup.sql
+++ prisma/setup.sql
@@ -8,3 +8,19 @@ CREATE TABLE users (
   salt TEXT NOT NULL,
   created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
+
+/* Lesson Table */
+CREATE TABLE lesson (
+  lesson_id SERIAL PRIMARY KEY,
+  title TEXT NOT NULL,
+  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+/* LessonQuestion Table */
+CREATE TABLE lessonQuestion (
+  question_id SERIAL PRIMARY KEY,
+  lesson_id INT NOT NULL,
+  question TEXT NOT NULL,
+  answer TEXT NOT NULL,
+  FOREIGN KEY (lesson_id) REFERENCES lesson(lesson_id)
+);
