04aae558a77957581e3923fb3eaf7603f60c529d
zhiyan114 <zhiyan114@zhiyan114.com>
2024-04-13 17:52:09 -0400

    Fixed sgv css clip-rule error

--- src/components/DashboardContent.tsx
+++ src/components/DashboardContent.tsx
@@ -14,7 +14,7 @@ export default function DashboardContent() {
         <div className="flex col-span-3 ">
           <button className="drop-shadow-lg bg-yellow-300 hover:bg-yellow-200 rounded-full self-center mx-2 px-1 py-1 my-1 hover:text-black">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="" className="fill-black w-8 h-8 ">
-              <path fill-rule="evenodd" d="M12 3.75a.75.75 0 0 1 .75.75v6.75h6.75a.75.75 0 0 1 0 1.5h-6.75v6.75a.75.75 0 0 1-1.5 0v-6.75H4.5a.75.75 0 0 1 0-1.5h6.75V4.5a.75.75 0 0 1 .75-.75Z" clip-rule="evenodd" />
+              <path fill-rule="evenodd" d="M12 3.75a.75.75 0 0 1 .75.75v6.75h6.75a.75.75 0 0 1 0 1.5h-6.75v6.75a.75.75 0 0 1-1.5 0v-6.75H4.5a.75.75 0 0 1 0-1.5h6.75V4.5a.75.75 0 0 1 .75-.75Z" clipRule="evenodd" />
             </svg>
           </button>
           <p className="text-2xl ms-2 font-bold col-span-3 self-center">Your Study Sets</p>
