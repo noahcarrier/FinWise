20fc7069de776800c35991e7384b53c633688bb0
Noah Carrier <67935880+noahcarrier@users.noreply.github.com>
2024-04-28 19:11:05 -0400

    Created API endpoint changes to get user lessons

--- src/app/card/route.ts
+++ src/app/card/route.ts
@@ -33,3 +33,58 @@ export async function POST(request: Request) {
     lesson: res,
   }), {status: 201});
 }
+
+
+export async function GET(request: Request) {
+  // Pull user session
+  const user = await getUserFromCache(cookies().get("session")?.value);
+  if(!user)
+    return new Response('Auth Required', {status: 402});
+
+  try {
+    const lessons = await getLessonsById(user.id);
+
+    return new Response(JSON.stringify(lessons), {status: 200});
+    
+  }
+  catch (error) {
+    console.error("Error fetching lessons:", error);
+    return new Response('Internal Server Error', {status: 500})
+  }
+
+  
+}
+
+export async function getLessonsById(userId: number) {
+  try {
+    const lessons = await prisma?.lesson.findMany({
+      where: {
+        user_id: userId,
+      }
+    });
+    return lessons;
+  }
+  catch (error) {
+    console.error("Error fetching lessons:", error);
+    return null;
+  }
+}
+
+/*
+export const getLessonById = async (lessonId: number) => {
+  try {
+    // Use Prisma to fetch the lesson based on its ID
+    const lesson = await prisma.lesson.findUnique({
+      where: {
+        id: lessonId,
+      },
+      include: {
+        lessonquestion: true, // Include associated questions with the lesson
+      },
+    });
+    return lesson;
+  } catch (error) {
+    console.error(error);
+    return null; // Return null if there's an error
+  }
+};*/
