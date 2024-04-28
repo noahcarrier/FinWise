c68574f0918312c45e8afd32b036d68e1ed43541
Kaevhi <ktotran27@gmail.com>
2024-04-28 19:24:12 -0400

    card changes to homepage

--- src/components/HomePageCards.tsx
+++ src/components/HomePageCards.tsx
@@ -3,7 +3,7 @@ export default function HomePageCards() {
   return (
     <div className="grid-cols-2 lg:grid xl:grid-cols-4">
   <div className="mx-9 mb-12 flex flex-col rounded-lg relative">
-    <div style={{ background: 'radial-gradient(circle, #FDE68A, #F59E0B)' }} className="rounded-lg z-10">
+    <div style={{ background: 'radial-gradient(circle, #FFDD00, #F59E0B)' }} className="rounded-lg z-10">
       <svg xmlns="http://www.w3.org/2000/svg" className="rounded-t-lg object-cover p-5" viewBox="0 0 512 512">
         <path d="M184 0c30.9 0 56 25.1 56 56V456c0 30.9-25.1 56-56 56c-28.9 0-52.7-21.9-55.7-50.1c-5.2 1.4-10.7 2.1-16.3 2.1c-35.3 0-64-28.7-64-64c0-7.4 1.3-14.6 3.6-21.2C21.4 367.4 0 338.2 0 304c0-31.9 18.7-59.5 45.8-72.3C37.1 220.8 32 207 32 192c0-30.7 21.6-56.3 50.4-62.6C80.8 123.9 80 118 80 112c0-29.9 20.6-55.1 48.3-62.1C131.3 21.9 155.1 0 184 0zM328 0c28.9 0 52.6 21.9 55.7 49.9c27.8 7 48.3 32.1 48.3 62.1c0 6-.8 11.9-2.4 17.4c28.8 6.2 50.4 31.9 50.4 62.6c0 15-5.1 28.8-13.8 39.7C493.3 244.5 512 272.1 512 304c0 34.2-21.4 63.4-51.6 74.8c2.3 6.6 3.6 13.8 3.6 21.2c0 35.3-28.7 64-64 64c-5.6 0-11.1-.7-16.3-2.1c-3 28.2-26.8 50.1-55.7 50.1c-30.9 0-56-25.1-56-56V56c0-30.9 25.1-56 56-56z" />
       </svg>
@@ -13,9 +13,7 @@ export default function HomePageCards() {
       </div>
     </div>
 
-
-
-      <div className="mx-9 mb-12 flex flex-col  rounded-lg bg-yellow-300 text-surface text-black ">
+      <div className="mx-9 mb-12 flex flex-col rounded-lg  text-surface text-black ">
       <div style={{ background: 'radial-gradient(circle, #FDE68A, #F59E0B)' }} className="rounded-lg z-10">
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="p-5">
           <path d="M11.7 2.805a.75.75 0 0 1 .6 0A60.65 60.65 0 0 1 22.83 8.72a.75.75 0 0 1-.231 1.337 49.948 49.948 0 0 0-9.902 3.912l-.003.002c-.114.06-.227.119-.34.18a.75.75 0 0 1-.707 0A50.88 50.88 0 0 0 7.5 12.173v-.224c0-.131.067-.248.172-.311a54.615 54.615 0 0 1 4.653-2.52.75.75 0 0 0-.65-1.352 56.123 56.123 0 0 0-4.78 2.589 1.858 1.858 0 0 0-.859 1.228 49.803 49.803 0 0 0-4.634-1.527.75.75 0 0 1-.231-1.337A60.653 60.653 0 0 1 11.7 2.805Z" />
@@ -30,7 +28,7 @@ export default function HomePageCards() {
       </div>
       </div>
 
-      <div className="mx-9 mb-12 flex flex-col  rounded-lg bg-yellow-300 text-surface text-black ">
+      <div className="mx-9 mb-12 flex flex-col rounded-lg  text-surface text-black ">
       <div style={{ background: 'radial-gradient(circle, #FDE68A, #F59E0B)' }} className="rounded-lg z-10">
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" className="p-5">
           <path d="M448 256A192 192 0 1 0 64 256a192 192 0 1 0 384 0zM0 256a256 256 0 1 1 512 0A256 256 0 1 1 0 256zm256 80a80 80 0 1 0 0-160 80 80 0 1 0 0 160zm0-224a144 144 0 1 1 0 288 144 144 0 1 1 0-288zM224 256a32 32 0 1 1 64 0 32 32 0 1 1 -64 0z" />
@@ -45,7 +43,7 @@ export default function HomePageCards() {
       </div>
       </div>
 
-      <div className="mx-9 mb-12 flex flex-col  rounded-lg bg-yellow-300 text-surface text-black ">
+      <div className="mx-9 mb-12 flex flex-col  rounded-lg  text-surface text-black ">
       <div style={{ background: 'radial-gradient(circle, #FDE68A, #F59E0B)' }} className="rounded-lg z-10">
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="p-5">
           <path fillRule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25ZM12.75 6a.75.75 0 0 0-1.5 0v6c0 .414.336.75.75.75h4.5a.75.75 0 0 0 0-1.5h-3.75V6Z" clipRule="evenodd" />
